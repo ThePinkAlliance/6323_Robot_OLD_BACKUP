1a21c107e093cc0154a59aa79286d5c150750744
Samuel Villegas <52802289+devsamuelv@users.noreply.github.com>
2023-12-07 21:19:38 -0500

    Removed robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp(name = "Driver Control")
 public class Tele extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
